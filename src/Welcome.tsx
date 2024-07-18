6229683654942ed55ff7bbd6a3c9de6499d4dd4d
SpandanBG <spandanbg@gmail.com>
2024-07-19 00:49:58 +0530

    update welcome

--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useRef } from 'react';
 import type { SSInfo } from './hooks/use-signaling-server'
 
 export interface WelcomeProps {
@@ -5,7 +6,25 @@ export interface WelcomeProps {
   setChatUI: () => void;
 }
 
-export function Welcome({ setChatUI }: WelcomeProps) {
-  return <>Welcome</>
+export function Welcome({ setChatUI, ssInfo }: WelcomeProps) {
+  const { add, join } = ssInfo;
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const joinGrp = useCallback(() => {
+    if (!inputRef.current) return;
+    add(inputRef.current.value)
+    join(inputRef.current.value)
+    setChatUI();
+  }, [add, join])
+
+  return (
+    <>
+      <button type="button" onClick={setChatUI}>Host</button>
+      <br />
+      <input ref={inputRef} type="text" placeholder="uuid" />
+      <button type="button" onClick={joinGrp}>Join</button>
+    </>
+  )
 }
 
