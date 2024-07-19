a48498d8065c15562674a38c240bec45d6db799c
Dibyendu <dibyendu.biswas@hotstar.com>
2024-07-20 01:06:17 +0530

    Added video chat feature

--- src/hooks/use-rtc.ts
+++ src/hooks/use-rtc.ts
@@ -1,12 +1,12 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
-import type { SSInfo } from './use-signaling-server';
+import { useCallback, useEffect, useRef, useState } from "react";
+import type { SSInfo } from "./use-signaling-server";
 import {
   offerPacket,
   answerPacket,
   iceCandidatePacket,
   unpackPacket,
   PacketType,
-} from '../lib/packet'
+} from "../lib/packet";
 
 const peerConfig: RTCConfiguration = {
   iceServers: [
@@ -32,14 +32,14 @@ const peerConfig: RTCConfiguration = {
     { urls: "stun:stun.xten.com" },
     {
       urls: "turn:localhost:3478",
-      username: 'asdf',
-      credential: 'asdf',
+      username: "asdf",
+      credential: "asdf",
     },
-  ]
-}
+  ],
+};
 
-const dataChannelName = "channel-123"
-const dataChannelOptions: RTCDataChannelInit = {}
+const dataChannelName = "channel-123";
+const dataChannelOptions: RTCDataChannelInit = {};
 
 interface PeerContext {
   sentIceCandidate: boolean;
@@ -49,27 +49,37 @@ interface PeersManager {
   peerContexts: Record<string, PeerContext>;
   sendIceToFreshPeer: (iceCandiates: RTCIceCandidate) => void;
 }
+function hasUserMedia() {
+  //check if the browser supports the WebRTC
+  return !!navigator.mediaDevices.getUserMedia;
+}
 
 function useManagedPeers(ssInfo: SSInfo): PeersManager {
-  const [peerCtx, setPeerCtx] = useState<Record<string, PeerContext>>({})
+  const [peerCtx, setPeerCtx] = useState<Record<string, PeerContext>>({});
 
   // Add new peer to ctx if present
   useEffect(() => {
-    ssInfo.peers?.forEach(peer => {
+    ssInfo.peers?.forEach((peer) => {
       if (peer in peerCtx) return;
-      setPeerCtx(prevCtx => ({ ...prevCtx, [peer]: { sentIceCandidate: false } }))
-    })
-  }, [ssInfo.peers.length])
+      setPeerCtx((prevCtx) => ({
+        ...prevCtx,
+        [peer]: { sentIceCandidate: false },
+      }));
+    });
+  }, [ssInfo.peers.length]);
 
-  const sendIceToFreshPeer = useCallback((iceCandidate: RTCIceCandidate) => {
-    Object.keys(peerCtx).forEach(peerUUID => {
-      if (peerCtx[peerUUID].sentIceCandidate) return;
-      ssInfo.send(iceCandidatePacket(iceCandidate, ssInfo.uuid), peerUUID)
-      peerCtx[peerUUID].sentIceCandidate = true;
-    })
-  }, [peerCtx])
+  const sendIceToFreshPeer = useCallback(
+    (iceCandidate: RTCIceCandidate) => {
+      Object.keys(peerCtx).forEach((peerUUID) => {
+        if (peerCtx[peerUUID].sentIceCandidate) return;
+        ssInfo.send(iceCandidatePacket(iceCandidate, ssInfo.uuid), peerUUID);
+        peerCtx[peerUUID].sentIceCandidate = true;
+      });
+    },
+    [peerCtx]
+  );
 
-  return { peerContexts: peerCtx, sendIceToFreshPeer }
+  return { peerContexts: peerCtx, sendIceToFreshPeer };
 }
 
 interface RTCInfo {
@@ -77,110 +87,208 @@ interface RTCInfo {
   createOffer: (uuid: string) => void;
   sendMsg: (msg: string) => void;
   rtcReady: boolean;
+  sendVideoFeed: () => void;
+  stopVideoFeed: () => void;
 }
 
 export function useRTC(ssInfo: SSInfo): RTCInfo {
-  const peerConn = useRef<RTCPeerConnection>(new RTCPeerConnection(peerConfig))
-  const dataChannel = useRef<RTCDataChannel | undefined>()
-  const [channelMsg, setChannelMsg] = useState<string>("")
-  const [rtcReady, setRtcReady] = useState<boolean>(false)
-  const [iceCandidate, setIceCandidate] = useState<RTCIceCandidate | undefined>();
+  const peerConn = useRef<RTCPeerConnection>(new RTCPeerConnection(peerConfig));
+  const dataChannel = useRef<RTCDataChannel | undefined>();
+  const [channelMsg, setChannelMsg] = useState<string>("");
+  const [rtcReady, setRtcReady] = useState<boolean>(false);
+  const [iceCandidate, setIceCandidate] = useState<
+    RTCIceCandidate | undefined
+  >();
   const { peerContexts, sendIceToFreshPeer } = useManagedPeers(ssInfo);
 
-  useEffect(() => {
-    if (!iceCandidate || !peerContexts) return;
-    sendIceToFreshPeer(iceCandidate);
-  }, [peerContexts, iceCandidate])
+  // useEffect(() => {
+  //   if (!iceCandidate || !peerContexts) return;
+  //   sendIceToFreshPeer(iceCandidate);
+  // }, [peerContexts, iceCandidate]);
 
   // Prepare data channel and send ice candidate
   useEffect(() => {
+    console.log("peer current", peerConn.current);
     dataChannel.current = peerConn.current.createDataChannel(
       dataChannelName,
-      dataChannelOptions,
-    )
+      dataChannelOptions
+    );
 
     dataChannel.current.onopen = () => {
-      setRtcReady(true)
-    }
+      setRtcReady(true);
+    };
 
     peerConn.current.onicecandidate = ({ candidate }) => {
-      if (candidate) setIceCandidate(candidate)
-    }
+      if (candidate) setIceCandidate(candidate);
+    };
 
     peerConn.current.ondatachannel = (event) => {
       const dataChannel = event.channel;
+      console.log("data channel changed", event);
+      dataChannel.onmessage = ({ data }) => setChannelMsg(data);
+    };
 
-      dataChannel.onmessage = ({ data }) => setChannelMsg(data)
-    }
+    // Used to renegotiate the connection to ensure that the remote peer receives the new track.
+    peerConn.current.onnegotiationneeded = async () => {
+      try {
+        const offer = await peerConn.current.createOffer();
+        await peerConn.current.setLocalDescription(offer);
+        const data = offerPacket(offer, ssInfo.uuid);
+        ssInfo.publish(data);
+      } catch (error) {
+        console.error("Error during negotiation", error);
+      }
+    };
+
+    peerConn.current.ontrack = (event) => {
+      const remoteVideo = document.getElementById(
+        "remoteVideo"
+      ) as HTMLVideoElement;
+      console.log("remoteVideo", event);
+
+      remoteVideo.srcObject = event.streams[0];
+      console.log("show vide", remoteVideo.srcObject);
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [peerConn.current])
+  }, [peerConn.current]);
 
   const createOffer = useCallback(() => {
-    peerConn.current.createOffer()
-      .then((offer) => {
-        const data = offerPacket(offer, ssInfo.uuid)
+    peerConn.current.createOffer().then((offer) => {
+      const data = offerPacket(offer, ssInfo.uuid);
+      ssInfo.publish(data);
+
+      peerConn.current.setLocalDescription(offer);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [peerConn.current]);
+
+  const handleIncomingOffer = useCallback(
+    (offer: RTCSessionDescriptionInit) => {
+      peerConn.current.setRemoteDescription(new RTCSessionDescription(offer));
+      peerConn.current.createAnswer().then((answer) => {
+        const data = answerPacket(answer, ssInfo.uuid);
         ssInfo.publish(data);
 
-        peerConn.current.setLocalDescription(offer)
-      })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [peerConn.current])
+        peerConn.current.setLocalDescription(answer);
+      });
 
-  const handleIncomingOffer = useCallback((offer: RTCSessionDescriptionInit) => {
-    peerConn.current.setRemoteDescription(new RTCSessionDescription(offer))
-    peerConn.current.createAnswer()
-      .then((answer) => {
-        const data = answerPacket(answer, ssInfo.uuid)
-        ssInfo.publish(data)
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    },
+    [peerConn.current]
+  );
 
-        peerConn.current.setLocalDescription(answer)
-      })
+  const handleIncomingAnswer = useCallback(
+    (answer: RTCSessionDescriptionInit) => {
+      peerConn.current.setRemoteDescription(new RTCSessionDescription(answer));
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    },
+    [peerConn.current]
+  );
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [peerConn.current])
-
-  const handleIncomingAnswer = useCallback((answer: RTCSessionDescriptionInit) => {
-    peerConn.current.setRemoteDescription(new RTCSessionDescription(answer))
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [peerConn.current])
-
-  const handleIncomingIceCandidate = useCallback((iceCandidate: RTCIceCandidate) => {
-    peerConn.current.addIceCandidate(iceCandidate)
-    setIceCandidate(iceCandidate);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [peerConn.current])
+  const handleIncomingIceCandidate = useCallback(
+    (iceCandidate: RTCIceCandidate) => {
+      peerConn.current.addIceCandidate(iceCandidate);
+      // setIceCandidate(iceCandidate);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    },
+    [peerConn.current]
+  );
 
   // Handle messages from signaling server
   useEffect(() => {
-    const [packet, isValid] = unpackPacket(ssInfo.msg)
+    const [packet, isValid] = unpackPacket(ssInfo.msg);
 
     if (!isValid) return;
 
     switch (packet.type) {
       case PacketType.OFFER:
-        handleIncomingOffer(packet.data as unknown as RTCSessionDescriptionInit)
+        handleIncomingOffer(
+          packet.data as unknown as RTCSessionDescriptionInit
+        );
         break;
       case PacketType.ANSWER:
-        handleIncomingAnswer(packet.data as unknown as RTCSessionDescriptionInit)
+        handleIncomingAnswer(
+          packet.data as unknown as RTCSessionDescriptionInit
+        );
         break;
       case PacketType.ICE_CANDIDATE:
-        handleIncomingIceCandidate(packet.data as unknown as RTCIceCandidate)
+        handleIncomingIceCandidate(packet.data as unknown as RTCIceCandidate);
         break;
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ssInfo.msg])
+  }, [ssInfo.msg]);
 
-  const sendMsg = useCallback((msg: string) => {
-    if (dataChannel.current && dataChannel.current.readyState) {
-      dataChannel.current.send(msg)
+  const sendMsg = useCallback(
+    (msg: string) => {
+      if (dataChannel.current && dataChannel.current.readyState) {
+        dataChannel.current.send(msg);
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [dataChannel.current]
+  );
+
+  const sendVideoFeed = useCallback(() => {
+    if (hasUserMedia()) {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: {
+            facingMode: "user", // or "environment" for rear camera
+          },
+          audio: true,
+        })
+        .then((stream) => {
+          const video = document.getElementById(
+            "localVideo"
+          ) as HTMLVideoElement;
+          if (video) {
+            video.srcObject = stream;
+            video.play();
+          }
+          stream.getTracks().forEach((track) => {
+            console.log("show tracks", track);
+            try {
+              peerConn.current.addTrack(track, stream);
+            } catch {
+              console.log("error adding track");
+            }
+            debugger;
+          });
+        })
+        .catch((error) => {
+          console.error("Error accessing media devices.", error);
+        });
+    } else {
+      console.error("User media not available");
     }
-  }, [dataChannel.current])
+  }, [peerConn.current]);
+
+  const stopVideoFeed = useCallback(() => {
+    const video = document.getElementById("localVideo") as HTMLVideoElement;
+    if (video && video.srcObject) {
+      const stream = video.srcObject as MediaStream;
+      const tracks = stream.getTracks();
+
+      tracks.forEach((track) => {
+        track.stop();
+      });
+
+      const senders = peerConn.current.getSenders();
+      senders.forEach((sender) => {
+        peerConn.current.removeTrack(sender);
+      });
+
+      video.srcObject = null;
+    }
+  }, []);
 
   return {
     channelMsg,
     createOffer,
     sendMsg,
     rtcReady,
-  }
+    sendVideoFeed,
+    stopVideoFeed,
+  };
 }
